9ac06a17a504d442f7434fa88a63cae24d42e306
rosmeryhanccoch <rosmerysistemas11@gmail.com>
2024-04-02 10:56:49 -0500

    Ejemplo

---
+++ PrincipiosPOO/src/main/java/pe/edu/upeu/poo/herencia/Vehicle.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.poo.herencia;
+
+/**
+ *
+ * @author Lab-IoT
+ */
+public class Vehicle {
+    protected static String marca = "Ford"; //attribute
+    
+public static void sonido() {//method
+System.out.println("Tuut, tuut!");
+}
+
+}

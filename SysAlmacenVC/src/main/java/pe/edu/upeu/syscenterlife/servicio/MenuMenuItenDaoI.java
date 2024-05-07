ab951432a8b58c41451bae170ec2cb7b806d3648
rosmeryhanccoch <rosmerysistemas11@gmail.com>
2024-05-07 11:19:51 -0500

    py inicio

---
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/servicio/MenuMenuItenDaoI.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pe.edu.upeu.syscenterlife.servicio;
+
+import java.util.List;
+import java.util.Properties;
+import pe.edu.upeu.syscenterlife.modelo.MenuMenuItenTO;
+
+/**
+ *
+ * @author Lab-IoT
+ */
+public interface MenuMenuItenDaoI {
+    public List<MenuMenuItenTO> listaAccesos(String perfil, Properties idioma);
+    
+}

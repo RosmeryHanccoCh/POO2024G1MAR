18e79bd3a1076e5d6da6f7220c0642d0cb5e22bc
rosmeryhanccoch <rosmerysistemas11@gmail.com>
2024-05-14 09:52:06 -0500

    crud

---
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/modelo/Cliente.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.syscenterlife.modelo;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Data
+public class Cliente {
+    @Id
+    @Basic(optional = false)
+    String dniruc;
+    String nombrers,documento;
+}
